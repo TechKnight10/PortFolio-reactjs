c36e5c1c6e7e5c1f5701f480cde7156812bee49a
TechKnight10 <89975943+TechKnight10@users.noreply.github.com>
2023-11-08 00:07:58 +0530

    Update Contact.js

--- src/Contact.js
+++ src/Contact.js
@@ -4,11 +4,11 @@ const Contact = () => {
   return (
     <div className="section">
       <h2>Contact Me</h2>
-      <p>Email: anuvrat.verma2021@vitbhopal.ac.in</p>
-      <p>Phone: +91 8849329261</p>
-      <p>LinkedIn: https://www.linkedin.com/in/anuvrat-verma/</p>
-      <p>Github: https://github.com/Anuvrat-Verma</p>
-      <p>Nationality: Indian</p>
+      <p>Email: Kalmit.kulkarni2021@vitbhopal.ac.in</p>
+      <p>Phone: +91 9324436718</p>
+      <p>LinkedIn: https://www.linkedin.com/in/kalmit-kulkarni-3378a421a/</p>
+      <p>Github: https://github.com/TechKnight10</p>
+      <p>Nationality:Indian</p>
     </div>
   );
 };
